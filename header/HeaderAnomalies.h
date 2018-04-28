4b8a310a37aa6ea4b526066b7757ba7fe38b9193
Daniellyson <etu32290@henallux.be>
2018-04-28 13:21:24 +0200

    header

---
+++ header/HeaderAnomalies.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define CALCULSIMPSON 75
+
+void detectionDesAnomalies(void);
+int obtention(int * alphaControl, int * alphaWarning, double * moyenne, double * ecartType);
+int obtentionUtilisateur(int alphaControl, int alphaWarning);
+double calculIntervalle(double * lowerLimit, double moyenne, double ecartType, int alpha, int n);
+FILE * lectureFichier();
+double sqrt(double valeur);
+double ecartTYpe(double variance);
+double rechercheA(double alpha);
+double calculSimpson(int valeurCalculSimpson, int zero, double a, double f);
+double valeurAbsolue(double alphaCalcule);
+void detectionDesErreurs(void);
+double obtentionModele(double * UCL, double * LCL, double * UWL, double * LWL);
+char contrileMoyenne(double UCL, double LCL, double UWL, double LWL, double moyenneATest);
