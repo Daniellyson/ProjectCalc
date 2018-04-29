d143fb210ba3fa22af42608aa66e9f27ed45c0e9
Daniellyson <etu32290@henallux.be>
2018-04-29 10:45:05 +0200

    new update

--- header/HeaderAnomalies.h
+++ header/HeaderAnomalies.h
@@ -7,6 +7,8 @@
 #define FIMODELE "fiModele.csv"
 #define FITEST "fiTest.csv"
 
+typedef double fonctionLoiNormal(double);
+
 void detectionDesAnomalies(void);
 int obtention(int * alphaControl, int * alphaWarning, double * moyenne, double * ecartType);
 int obtentionUtilisateur(int * alphaControl, int * alphaWarning);
@@ -15,9 +17,8 @@ FILE * lectureFichier();
 double sqrt(double valeur);
 double ecartType(double variance);
 double rechercheA(double alpha);
-double calculSimpson(int valeurCalculSimpson, int zero, double a, double (*fonctionLoiNormal)(void));
+double calculSimpson(int valeurCalculSimpson, int zero, double a, double (*fonctionLoiNormal)(double));
 double valeurAbsolue(double alphaCalcule);
 void detectionDesErreurs(void);
 double obtentionModele(double * UCL, double * LCL, double * UWL, double * LWL);
 char controleMoyenne(double UCL, double LCL, double UWL, double LWL, double moyenneATest);
-double fonctionLoiNormal(void);
